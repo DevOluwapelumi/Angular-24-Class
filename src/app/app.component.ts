4e8278ad9776616ebe16af217411065da731ae53
OGUNDIRAN VICTOR ISREAL <victoroluwadarasimi16@gmail.com>
2024-01-10 13:56:13 +0100

    typescript start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,26 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-class';
+
+  public firstname='Pelumi'
+  private lastname = 'Victor' //Property 'lastname' is private and only accessible within class 'AppComponent'.//
+  public age = 30
+  public boolean=true
+
+  //TypeScript below...//
+  public address:string='No 10, Ogbomoso, Oyo State, Nigeria'
+  public email: string = 'victor16@gmail.com'
+  public color: number = 70
+  public aged:undefined | number | string |boolean
+  
+  ngOnInit() {
+    // this.firstname='Tobi'
+    // this.aged= 20
+    // this.aged= 'Dev'
+    // this.aged= 'false'
+    console.log(this.firstname);
+  }
+
+  //TypeScript ---//
+  //Union, Inference, Interface
 }
