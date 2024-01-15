fd8bf9367badb50465401eec2b23d029b8152a85
OGUNDIRAN VICTOR ISREAL <victoroluwadarasimi16@gmail.com>
2024-01-15 14:08:18 +0100

    working with arrays

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+interface Objectname{
+  firstname: string,
+  lastname: string,
+  age:number
+}
+interface Objectname2{
+  email: string,
+  address: string,
+  firstname: string
+}
+interface Objectname3{
+  id: number,
+  name: string,
+}
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -11,6 +24,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-class';
+  
+  public school:any=false
 
   public firstname='Pelumi'
   private lastname = 'Victor' //Property 'lastname' is private and only accessible within class 'AppComponent'.//
@@ -23,14 +38,62 @@ export class AppComponent {
   public color: number = 70
   public aged:undefined | number | string |boolean
   
+  // public obj1:Objectname={
+  //   firstname: string,
+  //   lastname: string,
+  //   age:number
+  // } = {
+  //     firstname: 'Daniel',
+  //     lastname: 'Segun',
+  //     age: 70
+  // }
+
+
+  public obj1:Objectname={
+    firstname: 'Daniel',
+    lastname: 'Segun',
+    age:70
+  }
+  public obj2:Objectname2={
+    email: 'Daniel16@gmail.com',
+    address: 'Ogbomoso',
+    firstname:'Tolu'
+  }
+
+  public array1: number[] = [45, 724, 23, 232]
+  public array2: string[] = ['Oyin', 'Lola', 'Samuel', 'Victor']
+
+  public array3: Objectname3[] = [
+    { id: 1, name: 'John' },
+    { id: 2, name: 'Jane' }
+  ]
+
+  public mixedArray: (string | number | boolean)[] = ["Hello", 42, true, "World", false, 123];
+  
+  checkindex(index:number) {
+    console.log(index);
+    
+  }
+
   ngOnInit() {
     // this.firstname='Tobi'
     // this.aged= 20
     // this.aged= 'Dev'
     // this.aged= 'false'
     console.log(this.firstname);
-  }
+    
+    console.log("Array3:");
+    this.array3.forEach(array => {
+    console.log(`id: ${array.id}, name: ${array.name}`);
+});
+    
+    for (let i = 0; i < this.mixedArray.length; i++) {
+      console.log(this.mixedArray[i]);
+    };
+    
 
   //TypeScript ---//
   //Union, Inference, Interface
-}
+  };
+};
+
