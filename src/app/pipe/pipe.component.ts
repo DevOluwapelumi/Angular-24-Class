e1f936c69ca5e2d57304b39bb0385d9c56bb3d69
MultiMarkTechPro Media <victoroluwadarasimi16@gmail.com>
2024-02-19 14:04:17 +0100

    pipe commit

--- src/app/pipe/pipe.component.ts
+++ src/app/pipe/pipe.component.ts
@@ -1,12 +1,22 @@
+import { CurrencyPipe, DatePipe, LowerCasePipe, NgClass, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault, PercentPipe, SlicePipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pipe',
   standalone: true,
-  imports: [],
+  imports: [DatePipe, UpperCasePipe, LowerCasePipe, CurrencyPipe, SlicePipe, PercentPipe,],
   templateUrl: './pipe.component.html',
   styleUrl: './pipe.component.css'
 })
 export class PipeComponent {
+  public time = new Date()
+  public name = 'Oluwapelumi'
+  
+  public condition = 'Red'
+  public color1 = 'Red'
+  public color2='Blue'
 
+  changeColor() {
+    this.condition=this.color1?this.color2:this.color1
+  }
 }
